a81689760be03e10b209e0dea80e8bfafecc1b76
GAROSDEV <garzonoscar7@gmail.com>
2024-08-31 16:23:18 -0500

    feat: Add Products page component and styles

---
+++ src/pages/Products.jsx
@@ -0,0 +1,18 @@
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Products() {
+  return (
+    <div>
+      <Nav />
+      <Header />
+      <h1>Products</h1>
+      <p>
+        This is the products page. This is where the user will be able to view
+        products.
+      </p>
+      <Footer />
+    </div>
+  );
+}
