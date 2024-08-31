982a5ec685d0f20895fe3f96a1d677bccb3de45f
GAROSDEV <garzonoscar7@gmail.com>
2024-08-31 16:21:18 -0500

    feat: Add footer component and styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import whatsapp from "../assets/whats-app.svg";
+import instagram from "../assets/instagram.svg";
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <h3>Contáctanos</h3>
+        <a href="#" target="_blank">
+          <img className="whatsapp" src={whatsapp} alt="logo-whatsapp" />
+        </a>
+        <a
+          href="https://www.instagram.com/navisgamboa_nailsspa/"
+          target="_blank"
+        >
+          <img className="instagram" src={instagram} alt="logo-instagram" />
+        </a>
+      </footer>
+    </div>
+  );
+}
