a286af0d96da235af5dc66b273aef90abd26d692
GAROSDEV <garzonoscar7@gmail.com>
2024-08-31 16:23:01 -0500

    feat: Add Design page component and styles

---
+++ src/pages/Design.jsx
@@ -0,0 +1,18 @@
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Design() {
+  return (
+    <div>
+      <Nav />
+      <Header />
+      <h1>Design</h1>
+      <p>
+        This is the design page. This is where the user will be able to design
+        their own products.
+      </p>
+      <Footer />
+    </div>
+  );
+}
