151b06979df0b473bbed8a554ffa97a80a87add0
GAROSDEV <garzonoscar7@gmail.com>
2024-08-31 16:27:06 -0500

    feat: Add lazy loading for images in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,36 @@
+import { Link } from "react-router-dom";
+
 import Nav from "../components/Nav.jsx";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+
+import agenda from "../assets/calendar.svg";
+import diseño1 from "../assets/design1.png";
+import diseño2 from "../assets/design3.png";
+import diseño3 from "../assets/design4.png";
+import "../styles/Home.css";
 
 export default function Home() {
   return (
     <div>
       <Nav />
       <Header />
-      <h1>Home</h1>
-      <p>
-        This is the home page. This is where the user will be able to view
-        information about the website.
-      </p>
+
+      <div>
+        <Link to="/calendar" className="cita">
+          <img className="agenda" src={agenda} alt="logo-calendario" />
+          <h2 className="agenda">AGENDA TU CITA</h2>
+        </Link>
+        <section className="diseños">
+          <h2 className="titulo-diseños">DISEÑOS DE UÑAS</h2>
+          <div className="imagenes-diseños">
+            <img className="uñas" src={diseño1} alt="diseños de uñas" />
+            <img className="uñas" src={diseño2} alt="diseños de uñas" />
+            <img className="uñas" src={diseño3} alt="diseños de uñas" />
+          </div>
+        </section>
+      </div>
+
       <Footer />
     </div>
   );
