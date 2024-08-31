ae967e7d432a1330acd71b04dbb74bc07c9bcbd2
GAROSDEV <garzonoscar7@gmail.com>
2024-08-31 16:52:13 -0500

    feat: Update Calendar page with date selection and appointment scheduling functionality

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
+
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
+import "../styles/Calendar.css";
+
 import Nav from "../components/Nav";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function Calendar() {
+  const [selected, setSelected] = useState();
+
   return (
     <div>
       <Nav />
       <Header />
-      <h1>Calendar</h1>
-      <p>
-        This is the calendar page. This is where the user will be able to view
-        their calendar.
-      </p>
+
+      <div className="div-calendar">
+        <div className="calendar">
+          <DayPicker
+            mode="single"
+            selected={selected}
+            onSelect={setSelected}
+            footer={
+              selected
+                ? `Seleccionada: ${selected.toLocaleDateString()}`
+                : "Seleccione una fecha"
+            }
+          />
+        </div>
+      </div>
+      <div className="div-boton">
+        <button className="boton" type="submit">
+          Agendar Cita
+        </button>
+      </div>
+
       <Footer />
     </div>
   );
