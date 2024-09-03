9f1c6c41fc37306d82a359b1cd0e551f0d2bb0ff
GAROSDEV <garzonoscar7@gmail.com>
2024-09-02 21:27:00 -0500

    feat: Update navigation component with responsive menu functionality

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,50 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import img from "../assets/logo2.webp";
 import "../styles/Nav.css";
 
 export default function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div>
+    <nav className="nav-container">
       <div className="nav">
         <Link className="link" to="/">
           <img className="img" src={img} alt="Logo" />
         </Link>
-        <ul>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} size="2x" />
+        </div>
+        <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
           <li>
-            <Link className="link" to={"/products"}>
+            <Link className="link" to="/products">
               Productos
             </Link>
           </li>
           <li>
-            <Link className="link" to={"/design"}>
+            <Link className="link" to="/design">
               Diseños
             </Link>
           </li>
           <li>
-            <Link className="link" to={"/calendar"}>
+            <Link className="link" to="/calendar">
               Agenda
             </Link>
           </li>
           <li>
-            <Link className="link" to={"/session"}>
+            <Link className="link" to="/session">
               Iniciar Sesion
             </Link>
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 }
