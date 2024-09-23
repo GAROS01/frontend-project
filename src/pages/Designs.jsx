c2ab7bdbde09155c4955f2864877595a6ad7ec70
GAROSDEV <garzonoscar7@gmail.com>
2024-09-23 18:49:41 -0500

    Refactorizar bucle de diseño en Designs.jsx

--- src/pages/Designs.jsx
+++ src/pages/Designs.jsx
@@ -14,65 +14,9 @@ export default function Designs() {
       <section>
         <h2 className="titulo">Diseños</h2>
         <div className="design">
-          <Design
-            img={data.imagenes.imagen1.img}
-            alt={data.imagenes.imagen1.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen2.img}
-            alt={data.imagenes.imagen2.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen3.img}
-            alt={data.imagenes.imagen3.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen4.img}
-            alt={data.imagenes.imagen4.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen5.img}
-            alt={data.imagenes.imagen5.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen6.img}
-            alt={data.imagenes.imagen6.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen7.img}
-            alt={data.imagenes.imagen7.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen8.img}
-            alt={data.imagenes.imagen8.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen9.img}
-            alt={data.imagenes.imagen9.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen10.img}
-            alt={data.imagenes.imagen10.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen11.img}
-            alt={data.imagenes.imagen11.alt}
-          />
-
-          <Design
-            img={data.imagenes.imagen12.img}
-            alt={data.imagenes.imagen12.alt}
-          />
+          {data.imagenes.map((imagen, index) => (
+            <Design key={index} img={imagen.img} alt={imagen.alt} />
+          ))}
         </div>
         <p>Estos son algunos diseños realizados por nuestros especialistas</p>
       </section>
