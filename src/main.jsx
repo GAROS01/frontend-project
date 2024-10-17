0dc8a30b422707156954d1921614121cad2a64ec
GAROSDEV <garzonoscar7@gmail.com>
2024-10-17 11:48:28 -0500

    Refactorizar importación innecesaria de estilos en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
