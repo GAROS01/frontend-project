a0454e9b2c70adff6728d1af9197a95ba74cfa8f
GAROSDEV <garzonoscar7@gmail.com>
2024-10-17 16:23:07 -0500

    Refactorizar estilos de navegación y eliminar enlace de registro innecesario en Nav.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,7 +3,6 @@ import Home from "../pages/Home";
 import Products from "../pages/Products";
 import Design from "../pages/Designs";
 import Calendar from "../pages/Calendar";
-import Register from "../components/Register";
 import Session from "../pages/Session";
 
 export default function routes() {
@@ -14,7 +13,6 @@ export default function routes() {
         <Route path="/products" element={<Products />} />
         <Route path="/design" element={<Design />} />
         <Route path="/calendar" element={<Calendar />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/session" element={<Session />} />
       </Routes>
     </>
